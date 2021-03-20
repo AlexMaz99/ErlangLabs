22a57d8af071169d0f03051f0e73bcd133825605
AlexMaz99 <ola_m10@onet.pl>
2021-03-20 15:09:42 +0100

    Started lab2

---
+++ src/lab2/functions.erl
@@ -0,0 +1,24 @@
+%%%-------------------------------------------------------------------
+%%% @author Ola
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 20. mar 2021 12:45
+%%%-------------------------------------------------------------------
+-module(functions).
+-author("Ola").
+
+%% API
+-export([map/2, filter/2, digitsSum/1]).
+
+map(Fun, List) ->
+  [Fun(X) || X <- List].
+filter(Fun, List) ->
+  [X || X <- List, Fun(X)].
+
+digitsSum(X) ->
+  lists:foldl(fun (A, B) -> A + B -$0 end, 0, integer_to_list(X)).
+
+%% $0 - extracting the numerical value from digit symbol
+%% lists:filter(fun (X) -> functions:digitsSum(X) rem(3) == 0 end, [rand:uniform(25) || _ <- lists:seq(1, 1000000)]).
---
+++ src/lab2/qsort.erl
@@ -0,0 +1,32 @@
+%%%-------------------------------------------------------------------
+%%% @author Ola
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 20. mar 2021 12:45
+%%%-------------------------------------------------------------------
+-module(qsort).
+-author("Ola").
+
+%% API
+-export([lessThan/2, grtEqThan/2, qs/1, randomElems/3, compareSpeeds/3]).
+
+lessThan(List, Arg) ->
+  [X || X <- List, X < Arg].
+grtEqThan(List, Arg) ->
+  [X || X <- List, X >= Arg].
+qs([]) ->
+  [];
+qs([Pivot|Tail]) ->
+  qs(lessThan(Tail,Pivot)) ++ [Pivot] ++ qs(grtEqThan(Tail,Pivot)).
+
+randomElems(N, Min, Max) ->
+  [Min + rand:uniform(Max - Min) || _ <- lists:seq(1, N)].
+compareSpeeds(List, Fun1, Fun2) ->
+  Times = [timer:tc(X, [List]) || X <- [Fun1, Fun2]],
+  [Time1, Time2] = [X || {X, _} <- Times],
+  io:format("First function's time ~p~n", [Time1]),
+  io:format("Second function's time ~p~n", [Time2]).
+
+%% qsort:compareSpeeds(qsort:randomElems(100000, 1, 500), fun qsort:qs/1, fun lists:sort/1).
